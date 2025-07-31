5fc2e04b89925aac9bc3634d67bb312c628fa3ea
Xilthz156 <el.jason156@gmail.com>
2025-07-31 15:57:42 +0700

    Initialize CV2MySQL

---
+++ CSV2MySQL.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import os
+
+class ConvertCSV2MySQL:
+    def __init__(self, file_path_csv):
+        self.file_path_csv = file_path_csv
+        self.csv_df = pd.read_csv(self.file_path_csv)
+
+    def get_file_name(self):
+        return os.path.splitext(os.path.basename(self.file_path_csv))[0]
+    
+    def convert(self):
+        os.makedirs('output', exist_ok=True)
+
+        table_name = self.get_file_name()
+        table_columns = self.csv_df.columns.tolist()
+        values_list = []
+        for _, row in self.csv_df.iterrows():
+            values = []
+            for value in row:
+                if pd.isna(value) or value == '':
+                    values.append("NULL")
+                elif isinstance(value, (int, float)):
+                    integer_value = int(value) if value == int(value) else value
+                    values.append(str(integer_value))
+                else:
+                    value_str = str(value).replace("'", "\\'")
+                    values.append(f"{value_str}")
+        
+            columns_str = ', '.join(table_columns)
+            values_str = f"({', '.join(values)})"
+            values_list.append(values_str)
+            
+        group_values_str = ",\n".join(values_list)    
+        mysql_strng = f"INSERT INTO {table_name} ({columns_str}) VALUES\n{group_values_str};"
+
+        output_file_path = os.path.join('output', f"{table_name}.txt")
+        with open(output_file_path, 'w', encoding='utf-8') as writeMySQL:
+            writeMySQL.write(mysql_strng)
+
+        print(f"MySQL syntax has been saved to {output_file_path}.")
+
+if __name__ == "__main__":
+    csv_path = input("Input your CSV file directory (full path):\n")
+    cv2mysql = ConvertCSV2MySQL(csv_path)
+    cv2mysql.convert()
+                
---
+++ README.md
@@ -0,0 +1,15 @@
+# Converter for CSV to MySQL
+Converting CSV file into MySQL syntaxes for importing purposes.
+
+# Prequisite
+- Python 3.9.0
+- pandas  1.5.3
+- CSV file's name **MUST** be the same as your DB's table name.
+
+# How to use
+1. Copy the code of `CSV2MySQL.py`
+2. Create new .py file
+3. Open a terminal
+4. Run the Python script using `py CSV2MySQL.py`
+    - Note: Make sure your terminal is at the same directory as the python script.
+5. The script will create `output` folder with table name's.txt containing MySQL syntax.
